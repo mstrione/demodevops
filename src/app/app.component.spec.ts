be7f95f779b9e907840d720975b751d4d0aa1f8c
Mauro Strione <mauro.strione@gmail.com>
2018-10-24 19:29:12 -0300

    Fix final del test (karma), le faltaba una S

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Bienvenido a demodevops!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenidos a demodevops!');
   });
 });
