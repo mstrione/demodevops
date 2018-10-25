4c3b5175c0bb98bf5636e9c515e182d4839c25bf
Mauro Strione <mauro.strione@gmail.com>
2018-10-25 11:34:13 -0300

    Creo módulo vat-calculator

---
+++ src/app/vat-calculator/vat-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vat-calculator',
+  templateUrl: './vat-calculator.component.html',
+  styleUrls: ['./vat-calculator.component.sass']
+})
+export class VatCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
