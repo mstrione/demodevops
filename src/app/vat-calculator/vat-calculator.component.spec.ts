2209e2022ad8fce9f61993a644d55012d6905d7b
Mauro Strione <mauro.strione@gmail.com>
2018-11-29 14:40:57 -0300

    Elimino código que resuelve las 2 HdU del demo!

--- src/app/vat-calculator/vat-calculator.component.spec.ts
+++ src/app/vat-calculator/vat-calculator.component.spec.ts
@@ -59,26 +59,6 @@ describe('VatCalculatorComponent', () => {
     expect(compiled.querySelector('#afterTax').textContent).toContain('Valor con impuesto: 110');
   });
 
-  it ('#calculateTax should return 1150000 if value is 1000000', () => {
-    component.calculateTax(1000000);
-    expect(component.afterTax).toEqual(1150000);
-  });
 
-  it ('#calculateTax should return 1099998.9 if value is 999999', () => {
-    component.calculateTax(999999);
-    expect(component.afterTax).toEqual(1099998.9);
-  });
-
-  it ('#calculateTax should return 2300000 if value is 2000000', () => {
-    component.calculateTax(2000000);
-    expect(component.afterTax).toEqual(2300000);
-  });
-
-  it('should render "Impuesto: 10" in p with ID "tax" if value is 100', () => {
-    component.calculateTax(100);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('#tax').textContent).toContain('Impuesto: 10');
-  });
 
 });
