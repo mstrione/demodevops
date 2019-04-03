55e12453c94a60efe76a674c387a9c321263ee1c
mstrione <mauro.strione@gmail.com>
2019-04-03 09:41:30 -0300

    Elimino tests del 15%

--- src/app/vat-calculator/vat-calculator.component.spec.ts
+++ src/app/vat-calculator/vat-calculator.component.spec.ts
@@ -64,21 +64,5 @@ describe('VatCalculatorComponent', () => {
     expect(component.afterTax).toEqual(1099998.9);
   });
 
-  it ('#calculateTax should return 1150000 if value is 1000000', () => {
-    component.calculateTax(1000000);
-    expect(component.afterTax).toEqual(1150000);
-  });
-
-  it ('#calculateTax should return 2300000 if value is 2000000', () => {
-    component.calculateTax(2000000);
-    expect(component.afterTax).toEqual(2300000);
-  });
-
-  it('should render "Valor con impuesto: 2300000" in p with ID "afterTax" if value is 2000000', () => {
-    component.calculateTax(2000000);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('#afterTax').textContent).toContain('Valor con impuesto: 2300000');
-  });
 
 });
