8ebd63a9867ac9d5b718fb1d2e8a18e76f2a9810
mstrione <mauro.strione@gmail.com>
2019-04-03 20:42:57 -0300

    Historia mostrar impuestos por separado

--- src/app/vat-calculator/vat-calculator.component.spec.ts
+++ src/app/vat-calculator/vat-calculator.component.spec.ts
@@ -59,6 +59,14 @@ describe('VatCalculatorComponent', () => {
     expect(compiled.querySelector('#afterTax').textContent).toContain('Valor con impuesto: 110');
   });
 
+  it('should render "Impuesto: 10" in p with ID "tax" if value is 100', () => {
+    component.calculateTax(100);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#tax').textContent).toContain('Impuesto: 10');
+  });
+
+
   it ('#calculateTax should return 1099998.9 if value is 999999', () => {
     component.calculateTax(999999);
     expect(component.afterTax).toEqual(1099998.9);
